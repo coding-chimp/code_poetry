f8b9d2ead70c05d1ec4d224e776e9dc1fc3649b6
coding-chimp <xarfai27@gmail.com>
2014-05-02 19:42:09 +0200

    Rake task can take a path to an rails app.

--- lib/code_poetry/cli.rb
+++ lib/code_poetry/cli.rb
@@ -5,13 +5,32 @@ require 'code_poetry-html'
 
 module CodePoetry
   class CLI
-    def self.excecute
-      files = Array(FileList['app/**/*.rb']).compact
+    DIRECOTRIES = %w[app lib]
+    EXTENSIONS = %w[rb rake]
+
+    def self.excecute(directories = DIRECOTRIES)
+      files = Array(expand_directories_to_files(*directories)).compact
       calculator = Calculator.new(files)
       stats = calculator.calculate
 
       formatter = CodePoetry::Formatter::HTMLFormatter.new
       formatter.format(stats)
+    end
+
+  private
+
+    def self.expand_directories_to_files(*directories)
+      directories.flatten.map { |element|
+        if File.directory?(element)
+          Dir[expanded_diroctory_path(element)]
+        else
+          element
+        end
+      }.flatten.sort
+    end
+
+    def self.expanded_diroctory_path(directory)
+      File.join(directory, "{#{DIRECOTRIES.join(',')}}**", '**', "*.{#{EXTENSIONS.join(',')}}")
     end
   end
 end
